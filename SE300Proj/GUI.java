78622b6b2dc506e5d953d0e892b2efc87746f823
mpberk1 <berkowim@my.erau.edu>
2024-02-05 16:04:09 -0500

    Epic Gamer commit

--- SE300Proj/GUI.java
+++ SE300Proj/GUI.java
@@ -8,14 +8,14 @@ JFrame f= new JFrame("Example of TextField ");
 //variable declaration
 JTextField t12,t22;
 t12=new JTextField("EPIC GAMER Name!");
-t12.setBounds(10,100, 100,30);
+t12.setBounds(10,100, 250,30);
 //declaring new example
 t22=new JTextField("new example");
 //setting bounds
 t22.setBounds(10,150, 100,30);
 //adding into frames
 f.add(t12); f.add(t22);
-f.setSize(200,200);
+f.setSize(500,500);
 f.setVisible(true);
 }
 }
