326efc422213159b3dc8b4cd2fd44c1aeda6afa9
mpberk1 <berkowim@my.erau.edu>
2024-02-05 16:04:34 -0500

    commit

--- SE300Proj/GUI.java
+++ SE300Proj/GUI.java
@@ -4,17 +4,12 @@ class GUI
 {
 public static void main(String args[])  throws IOException
 {
-JFrame f= new JFrame("Example of TextField ");
-//variable declaration
-JTextField t12,t22;
+JFrame f= new JFrame("Plane Search ");
+JTextField t12;
 t12=new JTextField("EPIC GAMER Name!");
 t12.setBounds(10,100, 250,30);
-//declaring new example
-t22=new JTextField("new example");
-//setting bounds
-t22.setBounds(10,150, 100,30);
-//adding into frames
-f.add(t12); f.add(t22);
+
+f.add(t12);
 f.setSize(500,500);
 f.setVisible(true);
 }
