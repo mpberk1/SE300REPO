97f2668987ac3310960ac4c325012dda46b4faef
mpberk1 <berkowim@my.erau.edu>
2024-02-14 15:51:35 -0500

    Tabs

--- SE300Proj/GUI.java
+++ SE300Proj/GUI.java
@@ -6,8 +6,8 @@ public static void main(String args[])  throws IOException
 {
 JFrame f= new JFrame("Plane Search ");
 JTextField t12;
-t12=new JTextField("EPIC GAMER Name!");
-t12.setBounds(100,100, 250,300);
+t12=new JTextField("\t\tEPIC GAMER Name!");
+t12.setBounds(300,600, 250,300);
 
 f.add(t12);
 f.setSize(500,500);
