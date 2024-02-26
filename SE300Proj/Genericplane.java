63a19574afc5bdea0f733fa424fd419aa99edcbf
mpberk1 <berkowim@my.erau.edu>
2024-02-26 15:58:40 -0500

    Fixed bugs

--- SE300Proj/Genericplane.java
+++ SE300Proj/Genericplane.java
@@ -1,4 +1,8 @@
-public class Genericplane{
+public class Genericplane extends Genericparts{
+    public Genericplane(String Fuslage, String Engine, String Empanage) {
+        super(Fuslage, Engine, Empanage);
+        //TODO Auto-generated constructor stub
+    }
     String tailNumber;
     String planeType;
    
