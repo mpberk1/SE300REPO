799bad34a988a4b804a0b0addbc5efc6908c1bec
mpberk1 <berkowim@my.erau.edu>
2024-03-04 16:09:45 -0500

    Added Button

--- SE300Proj/GUI.java
+++ SE300Proj/GUI.java
@@ -1,18 +1,36 @@
 import javax.swing.*;
-import java.io.*;
-class GUI
-{
-public static void main(String args[])  throws IOException
-{
-JFrame f= new JFrame("Plane Search ");
-JTextField t12;
-t12=new JTextField("\t\tEPIC GAMER Name!");
-t12.setBounds(300,600, 250,300);
+import java.awt.event.*;
+import java.io.IOException;
+import java.awt.*;
 
-f.add(t12);
-String t12S = t12.getText();
-System.out.println(t12S);
-f.setSize(500,500);
-f.setVisible(true);
-}
+class GUI {
+    public static void main(String args[]) throws IOException {
+        JFrame f = new JFrame("Plane Search");
+        JTextField t12;
+        t12 = new JTextField("EPIC GAMER Name!");
+        t12.setHorizontalAlignment(JTextField.CENTER); // Center align the text
+        t12.setBounds(150, 200, 200, 30);
+
+        JButton grabButton = new JButton("Grab Text");
+        grabButton.setBounds(200, 250, 100, 30);
+
+        JTextArea outputArea = new JTextArea();
+        outputArea.setEditable(false);
+        outputArea.setBounds(150, 300, 200, 100);
+
+        grabButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String userSearchString = t12.getText();
+                outputArea.setText("Text grabbed: " + t12S);
+            }
+        });
+
+        f.add(t12);
+        f.add(grabButton);
+        f.add(outputArea);
+
+        f.setSize(500, 500);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
 }
