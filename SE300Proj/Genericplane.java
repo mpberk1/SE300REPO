ae6e75c3a7fcc219479ff54200e32fde2952ee43
mpberk1 <berkowim@my.erau.edu>
2024-03-06 14:02:22 -0500

    Revert "I'm gonna fix it later"
    This reverts commit c24cba49c59567e24e5a27f63feb60ae561a7ac1.

---
+++ SE300Proj/Genericplane.java
@@ -0,0 +1,12 @@
+public class Genericplane extends Genericparts{
+    public Genericplane(String Fuslage, String Engine, String Empanage) {
+        super(Fuslage, Engine, Empanage);
+        
+    }
+    String tailNumber;
+    String planeType;
+   
+
+
+
+}
