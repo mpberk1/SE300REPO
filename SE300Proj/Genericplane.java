c24cba49c59567e24e5a27f63feb60ae561a7ac1
JJangmanP <parkj89@my.erau.edu>
2024-03-07 03:26:29 +0900

    I'm gonna fix it later

--- SE300Proj/Genericplane.java
+++ SE300Proj/Genericplane.java
@@ -1,6 +1,6 @@
 public class Genericplane extends Genericparts{
-    public Genericplane(String Fuslage, String Engine, String Empanage) {
-        super(Fuslage, Engine, Empanage);
+    public Genericplane(String Fuslage, String Engine, String Empanage, String Wing) {
+        super(Fuslage, Engine, Empanage, Wing);
         
     }
     String tailNumber;
