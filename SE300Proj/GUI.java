96415ff1387a12f5c481d74735f7edb1b306fa69
mpberk1 <berkowim@my.erau.edu>
2024-03-09 12:00:02 -0500

    Made it pretty

--- SE300Proj/GUI.java
+++ SE300Proj/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
 
@@ -12,12 +13,13 @@ class GUI {
         CSVParser ob1 = new CSVParser();
         JTextField t12;
         t12 = new JTextField("Enter Tail Number");
-        t12.setHorizontalAlignment(JTextField.CENTER); // Center align the text
+        t12.setHorizontalAlignment(JTextField.CENTER);
         t12.setBounds(150, 50, 200, 30);
+        
 
         JTextField tUniqueCode;
         tUniqueCode = new JTextField("Enter Unique Code");
-        tUniqueCode.setHorizontalAlignment(JTextField.CENTER); // Center align the text
+        tUniqueCode.setHorizontalAlignment(JTextField.CENTER); 
         tUniqueCode.setBounds(150, 100, 200, 30);
 
         JButton grabButton = new JButton("Grab Text");
@@ -52,9 +54,14 @@ class GUI {
         f.add(closeButton);
         f.add(outputArea);
 
-        f.setSize(500, 700); // Increased window size
+        f.setSize(500, 700); 
         f.setLayout(null);
+        f.getContentPane().setBackground(new Color(135, 206, 235));
         f.setVisible(true);
+
+        // Change button color
+        UIManager.put("Button.background", UIManager.getColor("Panel.background")); 
+        UIManager.put("Button.foreground", UIManager.getColor("Panel.foreground")); 
     }
 
     public void setUserSearchString(String userSearchString) {
